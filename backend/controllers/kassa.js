80a423998585c7ab59c6a429d00c71cf90d8f56e
pelkrpxt <riku.p.pelkonen@student.jyu.fi>
2021-02-27 14:07:02 +0200

    removed unnecessary files

--- backend/controllers/kassa.js
+++
@@ -1,9 +0,0 @@
-const kassaRouter = require("express").Router()
-const Kassa = require("../models/kassa")
-
-kassaRouter.get("/", async (request, response) => {
-  const kassa = await Kassa.findOne()
-  console.log("kassa", kassa)
-  response.json(kassa)
-})
-module.exports = kassaRouter
